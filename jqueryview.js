8b41d60f78be3224dbaebb95d85e761669321311
Jurgen Leschner <jldec@ciaosoft.com>
2019-01-10 22:01:48 +0000

    bump page dependency, .npmrc, copyrights

--- jqueryview.js
+++ jqueryview.js
@@ -2,7 +2,7 @@
  * jqueryview.js
  *
  * pub-generator plugin for jquery views
- * copyright 2015, Jurgen Leschner - github.com/jldec - MIT license
+ * copyright 2015-2019, Jurgen Leschner - github.com/jldec - MIT license
  *
  * listens for 'nav', 'loaded', and 'updatedText' events
  * emits 'update-view' when content has been replaced
